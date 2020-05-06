e2f5742fe957c2ad713e4754f9df0b6f0b14de1f
Prasanna Biswas <probiswas82@gmail.com>
2020-05-07 05:09:46 +0530

    Added toggle-menu

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,21 +1,40 @@
-import React from 'react';
+import React,{Component} from 'react';
 
 import Aux from '../../hoc/Auxiliary';
 import classes from './Layout.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const layout =(props)=>(
-    <Aux>
-        <Toolbar />
-        <SideDrawer/>
+class Layout extends Component{
+    state={
+        showSideDrawer: false
+    }
+
+    sideDrawerClosedHandler=()=>{
+        this.setState({showSideDrawer: false});
+    }
+
+    sideDrawerToggleHandler = () => {
+        this.setState( ( prevState ) => {
+            return {showSideDrawer: !prevState.showSideDrawer};
+        } );
+    }
+
+    render(){
+        return (
+            <Aux>
+        <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler} />
+        <SideDrawer closed={this.sideDrawerClosedHandler}
+            open={this.state.showSideDrawer}/>
         <div>Toolbars, Sidebars, Backdrop</div>
         
         <main className={classes.Content}>
-            {props.children}
+            {this.props.children}
         </main>
 
     </Aux>
-);
+        );
+    }     
+}
 
-export default layout;
+export default Layout;
